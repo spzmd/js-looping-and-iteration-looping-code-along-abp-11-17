fc19c035c761b9a2b9a800b30e7a6466f81dc488
spzmd <b.keithdavid@gmail.com>
2017-11-23 23:25:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+
+/*const employee = ['Lisa', 'Kaitlin', 'Jan'];
+
+function printBadges (employee) {
+  for (let i = 0; i < employee.length; i++) {
+    console.log(`Welcome ${employee[i]}! You are employee #[i]` )
+  }
+}
+*/
+function printBadges(employee) {
+  for (let i = 0; i < employee.length ; i++) {
+    console.log(`Welcome ${employee[i]}! You are employee #${i+1}.`);
+  }
+  return employee;
+}
+
+printBadges();
+
+
+/*function tailsNeverFails() {
+  let counter = 0;
+  while (Math.random() >= 0.5) {
+    counter++;
+}
+  return `You got ${counter++} tail flips in a row!`;
+}
+*/
+function tailsNeverFails() {
+  let counter = 0;
+  while (Math.random() >= 0.5){
+    counter++;
+  }
+  return `You got ${counter++} tails in a row!`;
+}
